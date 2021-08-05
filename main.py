b34cdbdaa8edb13339728bd12926663a2b1c833d
Speccy-Rom <speccy.rom@gmail.com>
2021-08-05 14:17:32 +0300

    added connection db (postgresql)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from db.base import database
 import uvicorn
 
 app = FastAPI()
@@ -7,6 +8,16 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 if __name__ == '__main__':
