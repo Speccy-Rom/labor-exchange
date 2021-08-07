dfa70a5de7d9e78c538b68c66840d00126b349e8
Speccy-Rom <speccy.rom@gmail.com>
2021-08-07 17:58:39 +0300

    created repositories CRUD functional for users

--- repositories/users.py
+++ repositories/users.py
@@ -1,20 +1,60 @@
+import datetime
+
+from core.security import hash_password
 from db.users import users
+from typing import List, Mapping, Optional
+from models.user import User, UserIn
 from repositories.base import BaseRepository
 
 
 class UserRepository(BaseRepository):
 
-    async def get_all(limit: int = 100, skip: int = 0):
-        return
+    async def get_all(self, limit: int = 100, skip: int = 0) -> List[User]:
+        query = users.select().limit(limit).offset(skip)
+        return await self.database.fetch_all(query=query)
 
-    async def get_by_id(id: int):
-        return
+    async def get_by_id(self, id: int) -> User:
+        query = users.select().where(users.c.id == id).first()
+        user = await self.database.fetch_one(query)
+        if user is None:
+            return None
+        return User.parse_obj(user)
 
-    async def create():
-        return
+    async def create(self, u: UserIn) -> UserIn:
+        user = User(
+            name=u.name,
+            email=u.email,
+            hashed_password=hash_password(u.password),
+            is_company=u.is_company,
+            created_at=datetime.datetime.utcnow(),
+            updated_at=datetime.datetime.utcnow(),
+        )
+        values = {**user.dict()}
+        values.pop("id", None)
+        query = users.insert().values(**values)
+        user.id = await self.database.execute(query)
+        return user
 
-    async def update():
-        return
+    async def update(self, id: int, u: UserIn) -> UserIn:
+        user = User(
+            id=id,
+            name=u.name,
+            email=u.email,
+            hashed_password=hash_password(u.password2),
+            is_company=u.is_company,
+            created_at=datetime.datetime.utcnow(),
+            updated_at=datetime.datetime.utcnow(),
+        )
+        values = {**user.dict()}
+        values.pop("created_at", None)
+        values.pop("id", None)
+        query = users.update().where(users.c.id == id).values(**values)
+        await self.database.execute(query)
+        return user
 
-    async def get_by_email(email: str):
-        return
+    async def get_by_email(self, email: str) -> User:
+        query = users.select().where(users.c.email == email).first()
+        user = await self.database.fetch_one(query)
+        if user is None:
+            return None
+        return User.parse_obj(user)
