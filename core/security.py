cf58de68d29268564a3f6204328f0de3be3817af
Speccy-Rom <speccy.rom@gmail.com>
2021-09-07 19:02:23 +0300

    added jobs model and upd depends.py

--- core/security.py
+++ core/security.py
@@ -1,8 +1,9 @@
-from datetime import datetime
-from jose import jwt
-from core.config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
-
+import datetime
+from fastapi import Request, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from passlib.context import CryptContext
+from jose import jwt
+from config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -23,7 +24,23 @@ def create_access_token(data: dict) -> str:
 
 def decode_access_token(token: str):
     try:
-        encoded_jwt = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        encoded_jwt = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
     except jwt.JWSError:
         return None
     return encoded_jwt
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        exp = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid auth token")
+        if credentials:
+            token = decode_access_token(credentials.credentials)
+            if token is None:
+                raise exp
+            return credentials.credentials
+        else:
+            raise exp
