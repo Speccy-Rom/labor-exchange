6b8f0de2cd327f50deeb6a1292cdee70728d20ef
Speccy-Rom <speccy.rom@gmail.com>
2021-09-08 22:04:56 +0300

    added endpoints for jobs

--- endpoints/depends.py
+++ endpoints/depends.py
@@ -5,15 +5,18 @@ from db.base import database
 from core.security import JWTBearer, decode_access_token
 from models.user import User
 
+
 def get_user_repository() -> UserRepository:
     return UserRepository(database)
+
 
 def get_job_repository() -> JobRepository:
     return JobRepository(database)
 
+
 async def get_current_user(
-    users: UserRepository = Depends(get_user_repository),
-    token: str = Depends(JWTBearer()),
+        users: UserRepository = Depends(get_user_repository),
+        token: str = Depends(JWTBearer()),
 ) -> User:
     cred_exception = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Credentials are not valid")
     payload = decode_access_token(token)
